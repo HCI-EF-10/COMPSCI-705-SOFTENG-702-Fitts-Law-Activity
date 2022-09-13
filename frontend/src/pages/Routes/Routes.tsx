be0448f680e1be93ee33fc9fffee5afbd6748557
Hyung Park <hpar461@aucklanduni.ac.nz>
2022-09-13 17:33:12 +1200

    Add homepage

--- frontend/src/pages/Routes/Routes.tsx
+++ frontend/src/pages/Routes/Routes.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import { Route, Routes as Switch } from 'react-router-dom';
+import React from "react";
+import { Route, Routes as Switch } from "react-router-dom";
 import ExamplePage from "../ExamplePage/ExamplePage";
+import HomePage from "../HomePage";
 
 function Routes() {
-    return (
-        <Switch>
-            <Route path="/" element={<ExamplePage />}/>
-        </Switch>
-    );
+  return (
+    <Switch>
+      <Route path="/" element={<HomePage />} />
+      <Route path="demo" element={<ExamplePage />} />
+    </Switch>
+  );
 }
 
 export default Routes;
