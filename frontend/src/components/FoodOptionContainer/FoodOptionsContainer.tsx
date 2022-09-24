085131223d48f12b496b0f00665b73e68bde4747
DanilLinkov <danil.linkov@gmail.com>
2022-09-24 22:37:06 +1200

    made properties for the whole container instead of per item

--- frontend/src/components/FoodOptionContainer/FoodOptionsContainer.tsx
+++ frontend/src/components/FoodOptionContainer/FoodOptionsContainer.tsx
@@ -1,28 +1,30 @@
 import React from 'react';
-import {Grid} from "@mui/material";
+import { Grid } from "@mui/material";
 import FoodOptionsItem from "../FoodOptionsItem/FoodOptionsItem";
-import {FoodOption} from "../../util/Types/ApiTypes";
+import { FoodOption } from "../../util/Types/ApiTypes";
 
 interface Props {
   foodOptions: FoodOption[];
   onClick: () => void;
+  xSpacing: number;
+  ySpacing: number;
+  height: number;
+  width: number;
 }
 
-function FoodOptionsContainer({foodOptions, onClick}: Props) {
+function FoodOptionsContainer({foodOptions, onClick, xSpacing, ySpacing, height, width}: Props) {
 
   return (
-    <Grid container sx={{height: "500px", width: "500px"}}>
+    <Grid columnSpacing={xSpacing} rowSpacing={ySpacing} container>
       {foodOptions.map((foodOption, index) => (
         <FoodOptionsItem
+          height={height}
+          width={width}
           key={index}
           imgSrc={foodOption.imgSrc}
           title={foodOption.title}
           selected={foodOption.selected}
           onClick={onClick}
-          height={foodOption.height}
-          width={foodOption.width}
-          x={foodOption.x}
-          y={foodOption.y}
         />
       ))}
     </Grid>
