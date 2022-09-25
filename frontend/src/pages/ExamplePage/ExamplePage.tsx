a5c96a18bb0335ff583a15950540a5c0f5190d27
Aaron <61459012+asok3781@users.noreply.github.com>
2022-09-25 18:40:50 +1300

    Created wrapper for MUI tooltip for use in displaying popups

--- frontend/src/pages/ExamplePage/ExamplePage.tsx
+++ frontend/src/pages/ExamplePage/ExamplePage.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import CustomButton from "../../components/CustomButton/CustomButton";
 import MenuSlider from "../../components/MenuSlider/MenuSlider";
-import { Button } from "@mui/material";
-import HoverIcon from '../../components/HoverIcon/HoverIcon';
 import HelpIcon from '@mui/icons-material/Help';
+import HTMLTooltip from '../../components/HTMLTooltip/HTMLTooltip';
 
 function ExamplePage() {
   const [value, setValue] = React.useState(0);
@@ -11,9 +10,16 @@ function ExamplePage() {
   return (
     <>
       <CustomButton onClick={() => console.log("test")}>Test</CustomButton>
-      <div style={{width: "500px"}}>
-        <MenuSlider min={0} max={100} value={value} onChange={(value) => setValue(value)} menuText="Test"/>
-        <HoverIcon icon={<HelpIcon />} onHover={<div>test</div>} />
+      <div style={{ width: "500px" }}>
+        <MenuSlider min={0} max={100} value={value} onChange={(value) => setValue(value)} menuText="Test" />
+        <div style={{ display: 'flex', margin: '100px' }}>
+          <HTMLTooltip button={<HelpIcon />} htmlFrag={
+            <div>
+              <h1>Testing the hover functionality</h1>
+              Switching over from custom hover to MUI tooltips wrapper for a cleaner look with less styling
+            </div>} />
+        </div>
+
       </div>
     </>
   )
