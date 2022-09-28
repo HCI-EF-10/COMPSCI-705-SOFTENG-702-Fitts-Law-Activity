0bcc1b4c03883093b11654062930d9fdd12ab2ec
Hyung Park <hpar461@aucklanduni.ac.nz>
2022-09-28 16:29:03 +1300

    Rearrange settings container

--- frontend/src/components/SettingsContainer/SettingsContainer.tsx
+++ frontend/src/components/SettingsContainer/SettingsContainer.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Grid, Button } from "@mui/material";
+import React from "react";
+import { Grid, Button, Box, Typography, IconButton } from "@mui/material";
 import MenuSlider from "../../components/MenuSlider/MenuSlider";
 import { useNavigate } from "react-router-dom";
-
+import HelpIcon from "@mui/icons-material/Help";
 interface Props {
   menu: number;
 }
@@ -15,52 +15,122 @@ function SettingsContainer({ menu }: Props) {
   const [score, setScore] = React.useState(0);
   const [clicks, setClicks] = React.useState(0);
   const [errors, setErrors] = React.useState(0);
+  const [isEditMode, setIsEditMode] = React.useState(true);
   const navigate = useNavigate();
+
   const handleNavigate = () => {
     navigate("/");
   };
 
+  const setEditMode = () => {
+    setIsEditMode(true);
+  };
+
+  const setViewMode = () => {
+    setIsEditMode(false);
+  };
+
   return (
-    <Grid item container sx={{ display:"flex", flex: 2, backgroundColor: '#d4edfc' }}>
-      <Grid container
-        direction="column"
-        padding="15px">
-        <h1 style={{ textAlign: "center" }}>Settings</h1>
-        <h3>Selected component: Menu { menu } item</h3>
-        <h4>Component Properties</h4>
-      </Grid>
-      <Grid
-        container
-        direction="column"
-        justifyContent="space-evenly"
-        alignItems="stretch"
-        padding="15px">
-        <MenuSlider min={0} max={100} value={height} onChange={(height) => setHeight(height)} menuText="Height" />
-        <MenuSlider min={0} max={100} value={width} onChange={(width) => setWidth(width)} menuText="Width" />
-        <MenuSlider min={0} max={100} value={xPos} onChange={(xPos) => setXPos(xPos)} menuText="X Position" />
-        <MenuSlider min={0} max={100} value={yPos} onChange={(yPos) => setYPos(yPos)} menuText="Y Position" />
-      </Grid>
-      <Grid
-        container
-        direction="column"
-        justifyContent="center"
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        flex: 1.5,
+        backgroundColor: "#d4edfc",
+        paddingTop: "2rem",
+        paddingX: "1rem",
+      }}
+    >
+      <Box display="flex" flexDirection="row">
+        <Typography variant="h2">Settings</Typography>
+        <IconButton>
+          <HelpIcon fontSize="large" />
+        </IconButton>
+      </Box>
+      <Typography variant="subtitle1" marginTop="0.25rem">
+        Selected component: Menu {menu} item
+      </Typography>
+
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+          width: "100%",
+          marginTop: "4rem",
+        }}
+      >
+        <Typography variant="h3" marginBottom="1rem">
+          Component Properties
+        </Typography>
+        <Box
+          sx={{
+            width: "100%",
+          }}
+        >
+          <MenuSlider
+            min={0}
+            max={100}
+            value={width}
+            onChange={(width) => setWidth(width)}
+            menuText="Width"
+          />
+          <MenuSlider
+            min={0}
+            max={100}
+            value={height}
+            onChange={(height) => setHeight(height)}
+            menuText="Height"
+          />
+          <MenuSlider
+            min={0}
+            max={100}
+            value={xPos}
+            onChange={(xPos) => setXPos(xPos)}
+            menuText="X Position"
+          />
+          <MenuSlider
+            min={0}
+            max={100}
+            value={yPos}
+            onChange={(yPos) => setYPos(yPos)}
+            menuText="Y Position"
+          />
+        </Box>
+      </Box>
+      <Box
+        display="flex"
+        flexDirection="column"
         alignItems="center"
-        padding="15px">
-        <h2>Fitts’ Law Score: { score }</h2>
-        <h2>Number of clicks: { clicks }</h2>
-        <h2>Errors: { errors }</h2>
-      </Grid>
+        marginY="4rem"
+      >
+        <Typography variant="h3" display="flex">
+          Fitts’ Law Score: {score}
+        </Typography>
+        <Typography variant="h3">Number of clicks: {clicks}</Typography>
+        <Typography variant="h3">Errors: {errors}</Typography>
+      </Box>
       <Grid
         container
         justifyContent="space-evenly"
         alignItems="center"
-        padding="15px">
-        <Button variant='contained' onClick={handleNavigate}>EDIT MODE</Button>
-        <Button variant='contained' onClick={handleNavigate}>RUN TEST</Button>
-        <Button variant='contained' onClick={handleNavigate}>VIEW MODE</Button>
-        <Button variant='contained' onClick={handleNavigate}>INSTRUCTIONS</Button>
+        padding="15px"
+      >
+        {isEditMode ? (
+          <Button variant="contained" onClick={setViewMode}>
+            EDIT MODE
+          </Button>
+        ) : (
+          <Button variant="contained" onClick={setEditMode}>
+            VIEW MODE
+          </Button>
+        )}
+        <Button variant="contained" onClick={handleNavigate}>
+          RUN TEST
+        </Button>
       </Grid>
-    </Grid>
+    </Box>
   );
 }
 
